e31fc70fbe12673ba0ddbcf6092337685432d404
george iliadis <george.iliadis.91@gmail.com>
2023-12-05 23:35:09 +0200

    Day 4 part two done

--- day_four_part_two.cpp
+++ day_four_part_two.cpp
@@ -11,6 +11,10 @@ using namespace std;
 
 ifstream infile("input_day_four.txt");
 
+bool is_number(char c) {
+    return c >= '0' && c <= '9';
+}
+
 vector<string> split_string(const string& input, char delimiter) {
     vector<string> result;
     istringstream stream(input);
@@ -49,53 +53,61 @@ void parse_numbers(string card_numbers,set<int>& set_data){
     }   
 }
 
+int parse_number_from_string(string str){
+    string num_str = "";
+    for(int i = 0; i < str.length(); i++){
+        if(is_number(str[i])){
+            num_str += str[i];
+        }
+    }   
+    int n = stoi(num_str);
+    return n;
+}
+
 int main() {
     string line;
     unordered_map<int,int> card_amount_map;
 
-    while (getline(infile, line))
-    {
+    while (getline(infile, line)){
         vector<string> line_vec = split_string(line, ':');
         vector<string> nums_vec = split_string(line_vec[1], '|');
-        vector<string> game_label = split_string(line_vec[0], ' ');
+        int key_val = parse_number_from_string(line_vec[0]);
 
-        cout << game_label[0];
-
-        int key_val = stoi(game_label[1]);
         if(card_amount_map.find(key_val) == card_amount_map.end()){
             card_amount_map[key_val] = 1;
+        }else{
+            card_amount_map[key_val] += 1 ;
         }
 
         set<int> game_res_set;
         set<int> player_choice_set;
-
         parse_numbers(nums_vec[0],game_res_set);
         parse_numbers(nums_vec[1],player_choice_set);
 
         int score = 0 ;
-
         for (int num : player_choice_set) {
             if(game_res_set.count(num) > 0){
                 score++;
             }
         }
 
-        if(score > 0 ){
-            int card_copies = card_amount_map[key_val];
-            for(int i = 1; i <= score; i++){
-                int key = key_val + i;
-                if(card_amount_map.find(key) == card_amount_map.end()){
-                    card_amount_map[key] = card_copies;
-                }else{
-                    card_amount_map[key] += card_copies;
-                }
+        cout << "Game: " << key_val << " , " << score << endl;
+
+        int card_copies = card_amount_map[key_val];
+
+        for(int i = 1; i <= score; i++){
+            int key = key_val + i;
+
+            if(card_amount_map.find(key) == card_amount_map.end()){
+                card_amount_map[key] = card_copies;
+            }else{
+                card_amount_map[key] += card_copies;
             }
         }
 
         game_res_set.clear();
         player_choice_set.clear();
     }
-        cout<< "here" <<endl;
 
     int sum = 0 ;
     for (auto const& x : card_amount_map){
